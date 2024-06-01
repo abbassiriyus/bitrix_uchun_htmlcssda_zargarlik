d1787c7373c8eadac29a2b1b0070b2b9abb6f155
Fotimafgyt <159242143+Fotimafgyt@users.noreply.github.com>
2024-06-02 03:02:13 +0500

    12
    12

---
+++ pages/oneproduct/oneproduct.js
@@ -0,0 +1,24 @@
+function tovbtn() {
+    document.querySelector('.kuryer').style='display:block'
+    document.querySelector('.kuryer1').style='display:none'
+    document.querySelector('.kuryer2').style='display:none'
+    document.querySelector('.question').style='display:none'
+}
+function tovbtn1() {
+    document.querySelector('.kuryer').style='display:none'
+    document.querySelector('.kuryer1').style='display:block'
+    document.querySelector('.kuryer2').style='display:none'
+    document.querySelector('.question').style='display:none'
+}
+function tovbtn2() {
+        document.querySelector('.kuryer').style='display:none'
+        document.querySelector('.kuryer1').style='display:none'
+        document.querySelector('.kuryer2').style='display:block'
+    document.querySelector('.question').style='display:none'
+}
+function tovbtn3() {
+    document.querySelector('.kuryer').style='display:none'
+    document.querySelector('.kuryer1').style='display:none'
+    document.querySelector('.kuryer2').style='display:none'
+document.querySelector('.question').style='display:block'
+}
